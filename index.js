89c26fc78c7baa0f0b1fee1d4aa85eff6f864ff4
Amit Kumar <a.kbullet72@gmail.com>
2022-01-31 23:36:18 +0530

    Added a http call which returns a promise

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var http = require("http");
+var rest = require("rest");
 
 let server = http.createServer((req, res) => {
     console.dir(req, { depth: 0});
@@ -7,3 +8,16 @@ let server = http.createServer((req, res) => {
 
 server.listen(8081,() =>
     console.log('Server is running and listening to port 8081...'))
+
+fetchWebsite = () => rest('https://www.googlesdsdvsd.com');
+
+fetchWebsite().then(response => {
+    if (response.status.code === 200) {
+        console.log(`Successfully reached ${response.url}`);
+    }
+},
+error => {
+    console.log('Request failed with error: ',error.error);
+}
+);
+

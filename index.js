19e525878abfb4ae5754d8a791cce533f79603a8
Amit Kumar <a.kbullet72@gmail.com>
2022-02-01 21:44:03 +0530

    Added 2 additional promises

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 var http = require("http");
 var rest = require("rest");
+var when = require("when");
 
 let server = http.createServer((req, res) => {
-    console.dir(req, { depth: 0});
+    //console.dir(req, { depth: 0});
     res.end('Hello World');
 })
 
 server.listen(8081,() =>
     console.log('Server is running and listening to port 8081...'))
 
-fetchWebsite = () => rest('https://www.googlesdsdvsd.com');
+when.resolve('promise is resolved').then((result,error) => console.log(result));
+
+fetchPromise = (num) => {
+    return num/0;
+}
+
+when(fetchPromise(12)).then(result => console.log(result));
+
+
+fetchWebsite = () => rest('https://www.google.com');
 
 fetchWebsite().then(response => {
     if (response.status.code === 200) {
         console.log(`Successfully reached ${response.url}`);
+        process.exit(0)
     }
 },
 error => {
     console.log('Request failed with error: ',error.error);
 }
 );
+
 
